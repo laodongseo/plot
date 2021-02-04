85e57ad672b1487cef17a26ed12966e3ecdf5cb4
董董 <772483200@qq.com>
2021-02-04 15:58:28 +0800

    Update 1个画布1个ax多种数据条形图.py

--- 1个画布1个ax多种数据条形图.py
+++ 1个画布1个ax多种数据条形图.py
@@ -6,6 +6,8 @@ import numpy as np
 # 显示中文标签
 plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus']=False
+plt.rcParams['font.size'] = 14
+
 
 
 # 画图
@@ -21,6 +23,7 @@ def draw_pic(list_data_rects,labels,xlabel,ylabel,title,rect_width = 0.2):
     x_left = x_init - rect_width / 2 * (n - 1)
     y_left = list_data_rects[0]
     rect_left = ax.bar(x_left, list_data_rects[0][1], rect_width, label=list_data_rects[0][0])
+    autolabel(rect_left,ax)
 
     for i in range(1, n):
         x_now = x_left + rect_width * i
@@ -52,15 +55,16 @@ def autolabel(rects,ax):
 
 if __name__ == "__main__":
     # x轴的刻度标签;x标签,y标签,title
-    dic_lable = {'labels':['G1', 'G2', 'G3', 'G4', 'G5'],'xlabel':'哈哈','ylabel':'分数',
+    dic_lable = {'labels':['20200101', '20200102', '20200103', '20200104', '20200105'],'xlabel':'哈哈','ylabel':'分数',
      'title':'Scores by group and gender'}
 
-    # 分类的数据
+    # 分类数据-a类数据列表对应x轴刻度labels五个点的数据
     type1_data1 = ('a', [20, 34, 30, 35, 27])
     type2_data2 = ('b', [25, 32, 34, 20, 25])
     type3_data3 = ('c', [26, 38, 30, 18, 20])
     type4_data4 = ('d', [28, 35, 33, 13, 22])
     list_data_rects = [type1_data1, type2_data2, type3_data3, type4_data4]
+
 
     ax = draw_pic(list_data_rects,**dic_lable)
     plt.show()
