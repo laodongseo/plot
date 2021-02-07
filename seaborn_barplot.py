79543a2530e2b4ac76d41d3a77a0f208ee49b92a
董董 <772483200@qq.com>
2021-02-07 16:22:52 +0800

    Update seaborn_barplot.py

--- seaborn_barplot.py
+++ seaborn_barplot.py
@@ -7,16 +7,20 @@ plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 
 
-df = pd.read_excel('111.xlsx')
-ax = sns.barplot(x = 'top1-10', y = 'count',hue ='domains',data=df)
-ax.set(title='5i5j.com及竞品top1——10排名分布')
-# 添加数据标签
-for rect in ax.patches:
-    height = int(rect.get_height())
-    ax.annotate('{}%'.format(height),
-                xy=(rect.get_x() + rect.get_width() / 2, height),
-                xytext=(0, 0.2),  # 3 points vertical offset
-                textcoords="offset points",
-                ha='center', va='bottom')
-# plt.show()
-plt.savefig('./5.png')
+
+def draw_pic(x_col,y_col,hue_col,df_,title,pic_name):
+	ax = sns.barplot(x =x_col, y = y_col,hue =hue_col,data=df_)
+	ax.set(title=title)
+	# 添加数据标签
+	for rect in ax.patches:
+	    height = int(rect.get_height())
+	    ax.annotate('{}%'.format(height),
+	                xy=(rect.get_x() + rect.get_width() / 2, height),
+	                xytext=(0, 1.2),  # 3 points vertical offset
+	                textcoords="offset points",
+	                ha='center', va='bottom')
+	# plt.show()
+	plt.figure() # 连续保存多个图片用
+	fig = ax.get_figure()
+	ax.figure.savefig(f'{save_path}{pic_name}.png')
+	# plt.savefig(f'{save_path}{pic_name}.png')
